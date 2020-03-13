b79d56aa48928f88b24ad81c56cc0bc5cfdd708c
Aman Shah <aman.shah96.as@gmail.com>
2020-03-12 20:17:37 -0400

    assignmnet7

--- application_start.sh
+++ application_start.sh
@@ -9,4 +9,6 @@ cd /home/ubuntu/webapp/
 
 sudo kill -9 `lsof -i:8080 -t`
 
-nohup python3 views.py >  /dev/null
+#nohup python3 views.py >  /dev/null
+
+forever start -c python3 views.py
--- before_install.sh
+++ before_install.sh
@@ -4,3 +4,7 @@
 cd /opt/codedeploy-agent/deployment-root/deployment-instructions/
 sudo rm -rf *-cleanup
 
+cd
+
+sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo npm i -g forever --save
