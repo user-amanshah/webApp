0a1298e9b09f1166341c682f4678a99a92797e54
Aman Shah <aman.shah96.as@gmail.com>
2020-03-18 18:22:31 -0400

    assignmnet8

--- application_start.sh
+++ application_start.sh
@@ -7,14 +7,17 @@
 
 cd /home/ubuntu/webapp/
 
+
+orever stop -a -e error.log -a  -l loger.log -c python3 views.py
+
 sudo kill -9 `lsof -i:8080 -t`
 
-forever stop -a -e error.log -a  -l loger.log -c python3 ../webapp/views.py
+f
 
 
 #nohup python3 views.py >  /dev/null
 
-forever start -a -e error.log -a  -l loger.log -c python3 ../webapp/views.py
+forever start -a -e error.log -a  -l loger.log -c python3 views.py
 
 sudo chmod 776 Am
 #start cloudwatch
