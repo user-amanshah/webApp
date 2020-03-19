83192b7dee3e17ce90f54e0a0c420f4094c5a980
Aman Shah <aman.shah96.as@gmail.com>
2020-03-19 02:56:36 -0400

    assn8

--- before_install.sh
+++ before_install.sh
@@ -6,11 +6,13 @@ sudo rm -rf *-cleanup
 
 cd
 sudo apt-get update
-sudo apt-get install -y nodejs
 sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
 sudo npm i -g forever --save
 
 
 cd
 pwd
+
+
 
