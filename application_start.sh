377af161126f967edf485fc70792b88bae35d331
Aman Shah <aman.shah96.as@gmail.com>
2020-03-26 10:25:17 -0400

    assignment8

--- application_start.sh
+++ application_start.sh
@@ -8,22 +8,28 @@
 cd /home/ubuntu/webapp/
 
 
-forever stop -a -e loger.log -a  -l loger.log -c python3 views.py
+forever stop -a -e error.log -a  -l loger.log -c python3 views.py
+
+sleep 2
 
 sudo kill -9 `lsof -i:8080 -t`
+sleep 2
 
 
 
 
 #nohup python3 views.py >  /dev/null
 
-forever start -a -e loger.log -a  -l loger.log -c python3 views.py
+forever start -a  -l loger.log -c python3 views.py
 
+sleep 2
 
-#start cloudwatch
+forever start -a  -l loger.log -c python3 views.py
+#start cloudwatch####
 
 cd ../.forever
 
+pwd
 sudo chmod 776 loger.log
 
 
@@ -32,4 +38,4 @@ sudo mv loger.log /opt/
 
 cd
 
-#sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/AmazonCloudWatch-config.json -s
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/AmazonCloudWatch-config.json -s
