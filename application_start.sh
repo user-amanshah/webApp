11543f6c92a4366477365695a0514d41a59b7f4c
Aman Shah <aman.shah96.as@gmail.com>
2020-04-03 09:40:38 -0400

    assignment10-comment

--- application_start.sh
+++ application_start.sh
@@ -39,3 +39,6 @@ sudo mv loger.log /opt/
 cd
 
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/AmazonCloudWatch-config.json -s
+
+
+#comment
